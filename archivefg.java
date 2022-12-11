a84c62ece1af8d0869b9aca783fba85cbf53345c
Tom Cunningham <tcunning@redhat.com>
2022-12-11 17:19:03 -0500

    Add filtering for prospects

--- archivefg.java
+++ archivefg.java
@@ -18,9 +18,13 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.ScriptResult;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -61,6 +65,53 @@ public class archivefg {
         }
     }
 
+    public String makeTR(String line) {
+        StringBuffer sb = new StringBuffer();
+        String[] tokens = line.split(",");
+        if (tokens[tokens.length-1].startsWith("\"sa")) {
+            sb.append("<tr>");
+            for (String token : tokens) {
+                token = token.replaceAll("\"", "");
+                sb.append("<td>" + token + "</td>");
+            }
+            sb.append("</tr>");
+        }
+        return sb.toString();
+    }
+
+    public void filterForProspects(String filename) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String line = null;
+
+        String outputFileName = filename.replace(".csv", ".html").replace("steamer", "prospects");
+        PrintWriter pw = new PrintWriter(new FileOutputStream(outputFileName), true);
+        pw.println("<html>\n<table class=\"sortable\">");
+
+        // We want the header line
+        line = br.readLine();
+        String[] tokens = line.split(",");
+        StringBuffer sb = new StringBuffer();
+        sb.append("<thead><tr>");
+        for (String token : tokens) {
+            token = token.replaceAll("\"", "");
+            sb.append("<th>" + token + "</th>");
+        }
+        sb.append("</tr></thead>");
+        pw.println(sb.toString());
+
+        while ((line = br.readLine()) != null) {
+            String tr = makeTR(line);
+            if (!tr.isEmpty()) {
+                pw.println(tr);
+            }
+        }
+        pw.println("<link href=\"https://cdn.jsdelivr.net/gh/tofsjonas/sortable/sortable.min.css\" rel=\"stylesheet\" />");
+        pw.println("<script src=\"https://cdn.jsdelivr.net/gh/tofsjonas/sortable/sortable.min.js\"></script>");
+        pw.println("</table>\n</html>");
+        pw.flush();
+        pw.close();
+    }
+
     public static void main(String... args) throws Exception {
         System.getProperties().put("org.apache.commons.logging.simplelog.defaultlog", "error");
 
@@ -74,8 +125,17 @@ public class archivefg {
         String pitching = directory + File.separator + "steamerpitching.csv";
 
         archivefg fg = new archivefg();
-        fg.postForm("https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=steamer", offense);
-        fg.postForm("https://www.fangraphs.com/projections.aspx?pos=all&stats=pit&type=steamer&team=0&lg=all&players=0", pitching);
 
+        // Check whether file exists already
+        File offFile = new File(offense);
+        if (!offFile.exists()) {
+            fg.postForm("https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=steamer", offense);
+        }
+        File pitchingFile = new File(pitching);
+        if (!pitchingFile.exists()) {
+            fg.postForm("https://www.fangraphs.com/projections.aspx?pos=all&stats=pit&type=steamer&team=0&lg=all&players=0", pitching);
+        }
+        fg.filterForProspects(offense);
+        fg.filterForProspects(pitching);
     }
 }
